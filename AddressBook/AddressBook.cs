d7d77151d173603090d6ae1d89e9fe23025763c6
Mabel Christina <maybelchristina@gmail.com>
2021-09-27 20:04:11 +0530

    [Mabel] Add: UC3 added

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -26,6 +26,71 @@ namespace AddressBook
             contacts.PhoneNumber = Convert.ToInt64(Console.ReadLine());
             Console.WriteLine("Enter Email");
             contacts.Email = Console.ReadLine();
+            addcontacts.Add(contacts);
+        }
+        public void DisplayContact()
+        {
+            CreateContacts();
+            foreach (var data in addcontacts)
+            {
+                Console.WriteLine("The Contact Details are\n:" + data.Firstname + " " + data.LastName + " " + data.Address + " " + data.City + " " + data.State + " " + data.Zip + " " + data.PhoneNumber + " " + data.Email);
+            }
+        }
+        public void EditContact()
+        {
+            CreateContacts();
+            Console.WriteLine("to edit contact list enter contact firstname");
+            string name = Console.ReadLine().ToLower();
+            foreach (var data in addcontacts)
+            {
+                if (addcontacts.Contains(data))
+                {
+                    if (data.Firstname.Equals(name))
+                    {
+                        Console.WriteLine("To edit contacts enter 1.LastName\n 2.Address\n 3.City\n 4.State\n 5.Zip\n 6.PhoneNumber\n 7.Email\n");
+                        int options = Convert.ToInt32(Console.ReadLine());
+                        switch (options)
+                        {
+                            case 1:
+                                string lastname = Console.ReadLine();
+                                break;
+                            case 2:
+                                string address = Console.ReadLine();
+                                data.Address = address;
+                                break;
+                            case 3:
+                                string city = Console.ReadLine();
+                                data.City = city;
+                                break;
+                            case 4:
+                                string state = Console.ReadLine();
+                                data.State = state;
+                                break;
+                            case 5:
+                                string zip = Console.ReadLine();
+                                data.Zip = zip;
+                                break;
+                            case 6:
+                                int number = Convert.ToInt32(Console.ReadLine());
+                                data.PhoneNumber = number;
+                                break;
+                            case 7:
+                                string email = Console.ReadLine();
+                                data.Email = email;
+                                break;
+                            default:
+                                Console.WriteLine("choose valid option");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("name doesnt matches");
+                    }
+                }
+            }
+            DisplayContact();
         }
     }
+    
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,8 +6,29 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            AddressBook addressBook = new AddressBook();
-            addressBook.CreateContacts();
+            bool value = false;
+            int options;
+            while (!value)
+            {
+                Console.WriteLine("1.CreateContacts\n 2.DisplayContact \n 3.EditContacts\n ");
+                options = Convert.ToInt32(Console.ReadLine());
+                AddressBook address = new AddressBook();
+                switch (options)
+                {
+                    case 1:
+                        address.CreateContacts();
+                        break;
+                    case 2:
+                        address.DisplayContact();
+                        break;
+                    case 3:
+                        address.EditContact();
+                        break;
+                    default:
+                        Console.WriteLine("Choose valid option");
+                        break;
+                }
+            }
         }
     }
 }
